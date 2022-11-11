e9ce66d85345c698413b70550253a5548c74a13b
Ivanova Stacey <stacey_ivanova@mail.ru>
2022-11-11 21:45:43 +0300

    first variant

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,49 +6,51 @@ import { Link } from "react-router-dom";
 
 function Login(props) {
   return (
-    <form action="/movies" className="form__container" noValidate>
-      <Link to="/">
-        <img className="form__logo" src={logo} alt="логотип сайта" />
-      </Link>
-      <p className="form__title">Рады видеть!</p>
-      <label className="form__field">
-        <p className="form__text">E-mail</p>
-        <input
-          className="form__item"
-          name="email"
-          type="email"
-          id="email-input"
-        />
-        <span className="form__item-error email-input-error">
-          Необходимо ввести e-mail
-        </span>
-      </label>
-      <label className="form__field">
-        <p className="form__text">Пароль</p>
-        <input
-          type="password"
-          className="form__item"
-          name="password"
-          id="pass-input"
-          minLength="7"
-        />
-        <span className="form__item-error pass-input-error">
-          Слишком короткий пароль
-        </span>
-      </label>
-      <button
-        className="form__submit-button form__submit-button_signin_m"
-        type="submit"
-      >
-        Войти
-      </button>
-      <p className="form__subtext">
-        Еще не зарегистрированы?{" "}
-        <Link className="form__link" to="/signup">
-          Регистрация
+    <div class="form">
+      <form action="/movies" className="form__container" noValidate>
+        <Link to="/">
+          <img className="form__logo" src={logo} alt="логотип сайта" />
         </Link>
-      </p>
-    </form>
+        <p className="form__title">Рады видеть!</p>
+        <label className="form__field">
+          <p className="form__text">E-mail</p>
+          <input
+            className="form__item"
+            name="email"
+            type="email"
+            id="email-input"
+          />
+          <span className="form__item-error email-input-error">
+            Необходимо ввести e-mail
+          </span>
+        </label>
+        <label className="form__field">
+          <p className="form__text">Пароль</p>
+          <input
+            type="password"
+            className="form__item"
+            name="password"
+            id="pass-input"
+            minLength="7"
+          />
+          <span className="form__item-error pass-input-error">
+            Слишком короткий пароль
+          </span>
+        </label>
+        <button
+          className="form__submit-button form__submit-button_signin_m"
+          type="submit"
+        >
+          Войти
+        </button>
+        <p className="form__subtext">
+          Еще не зарегистрированы?{" "}
+          <Link className="form__link" to="/signup">
+            Регистрация
+          </Link>
+        </p>
+      </form>
+    </div>
   );
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,59 +5,61 @@ import { Link } from "react-router-dom";
 
 function Register(props) {
   return (
-    <form action="/signin" className="form__container" noValidate>
-      <Link to="/">
-        <img className="form__logo" src={logo} alt="логотип сайта" />
-      </Link>
-      <p className="form__title">Добро пожаловать!</p>
-      <label className="form__field">
-        <p className="form__text">Имя</p>
-        <input
-          className="form__item"
-          name="name"
-          id="name-input"
-          minLength="5"
-          maxLength="15"
-        />
-        <span className="form__item-error name-input-error">
-          Необходимо ввести имя
-        </span>
-      </label>
-      <label className="form__field">
-        <p className="form__text">E-mail</p>
-        <input
-          className="form__item"
-          name="email"
-          type="email"
-          id="email-input"
-        />
-        <span className="form__item-error email-input-error">
-          Необходимо ввести e-mail
-        </span>
-      </label>
-      <label className="form__field">
-        <p className="form__text">Пароль</p>
-        <input
-          type="password"
-          className="form__item"
-          name="password"
-          id="pass-input"
-          minLength="7"
-        />
-        <span className="form__item-error pass-input-error">
-          Слишком короткий пароль
-        </span>
-      </label>
-      <button className="form__submit-button" type="submit">
-        Зарегистрироваться
-      </button>
-      <p className="form__subtext">
-        Уже зарегистрированы?{" "}
-        <Link className="form__link" to="/signin">
-          Войти
+    <div class="form">
+      <form action="/signin" className="form__container" noValidate>
+        <Link to="/">
+          <img className="form__logo" src={logo} alt="логотип сайта" />
         </Link>
-      </p>
-    </form>
+        <p className="form__title">Добро пожаловать!</p>
+        <label className="form__field">
+          <p className="form__text">Имя</p>
+          <input
+            className="form__item"
+            name="name"
+            id="name-input"
+            minLength="5"
+            maxLength="15"
+          />
+          <span className="form__item-error name-input-error">
+            Необходимо ввести имя
+          </span>
+        </label>
+        <label className="form__field">
+          <p className="form__text">E-mail</p>
+          <input
+            className="form__item"
+            name="email"
+            type="email"
+            id="email-input"
+          />
+          <span className="form__item-error email-input-error">
+            Необходимо ввести e-mail
+          </span>
+        </label>
+        <label className="form__field">
+          <p className="form__text">Пароль</p>
+          <input
+            type="password"
+            className="form__item"
+            name="password"
+            id="pass-input"
+            minLength="7"
+          />
+          <span className="form__item-error pass-input-error">
+            Слишком короткий пароль
+          </span>
+        </label>
+        <button className="form__submit-button" type="submit">
+          Зарегистрироваться
+        </button>
+        <p className="form__subtext">
+          Уже зарегистрированы?{" "}
+          <Link className="form__link" to="/signin">
+            Войти
+          </Link>
+        </p>
+      </form>
+    </div>
   );
 }
 
