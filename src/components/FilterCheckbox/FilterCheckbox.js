ff4a0c0680e205a745e1349cbbc13bdfb2157b98
Ivanova Stacey <stacey_ivanova@mail.ru>
2022-11-11 23:20:14 +0300

    first variant1

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./FilterCheckbox.css";
 
 function FilterCheckbox(props) {
-  // return ()
+  return (
+    <div className="checkbox__container">
+      <label className="checkbox">
+        <input type="checkbox" className="hidden-checkbox"></input>
+        <span class="visible-checkbox"></span>
+      </label>
+      <p className="checkbox__text">Короткометражки</p>
+    </div>
+  );
 }
 
 export default FilterCheckbox;
