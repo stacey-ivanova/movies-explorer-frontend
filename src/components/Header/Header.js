038c80547a3ce1a01a38dc0b35b96f1f548adf61
Ivanova Stacey <stacey_ivanova@mail.ru>
2022-11-12 00:33:14 +0300

    first variant2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../images/logo.svg";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 import NavigationMain from "../NavigationMain/NavigationMain";
 import NavigationMovie from "../NavigationMovie/NavigationMovie";
 
+function changeHeader(props) {
+  const location = useLocation();
+  if (
+    location.pathname === "/movies" ||
+    location.pathname === "/saved-movies" ||
+    location.pathname === "/profile"
+  ) {
+    return "header header_type_black";
+  }
+  return "header";
+}
+
 function Header(props) {
   return (
-    <header className="header">
+    <header className={changeHeader()}>
       <Link to="/">
         <img className="header__logo" src={logo} alt="логотип сайта" />
       </Link>
